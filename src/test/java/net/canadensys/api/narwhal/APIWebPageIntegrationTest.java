69e968d56f3dc081b1093638cdfdbbb417031bce
cgendreau <christiangendreau@gmail.com>
2013-04-04 11:35:51 -0400

    better integration tests writing

--- src/test/java/net/canadensys/api/narwhal/APIWebPageIntegrationTest.java
+++ src/test/java/net/canadensys/api/narwhal/APIWebPageIntegrationTest.java
@@ -1,26 +1,32 @@
 package net.canadensys.api.narwhal;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 /**
  * Integration tests of the rendered pages.
  * @author canadensys
  *
  */
-public class APIWebPageIntegrationTest {
-	//TODO : move to a test config file
-	private static final String TESTING_SERVER_URL = "http://localhost:9966/tools/";
+public class APIWebPageIntegrationTest extends AbstractIntegrationTest {
 	
-	private WebDriver browser;
-	 
+	@FindBy(css = "form.narwhal-form textarea[name='data']")
+	private WebElement dataTextArea;
+	
+	@FindBy(css = "form.narwhal-form input[type='submit']")
+	private WebElement submit;
+	
+	@FindBy(css = "table.narwhal-results tbody")
+	private WebElement resultTableBody;
+	
 	@Before
 	public void setup() {
 		browser = new FirefoxDriver();
@@ -31,33 +37,34 @@ public class APIWebPageIntegrationTest {
 		//Coordinates is the landing page
 		browser.get(TESTING_SERVER_URL);
 		
-		browser.findElement(By.name("data")).sendKeys("2 | 40°26′47″N,74° 0' 21.5022\"W");
-		browser.findElement(By.tagName("input")).click();
+		//bind the WebElement to the current page
+		PageFactory.initElements(browser, this);
 		
-		WebElement resultTable = browser.findElement(By.className("narwhal-results"));
-		
-		assertEquals("40.4463889", resultTable.findElement(By.cssSelector("tbody td:nth-child(3)")).getText());
-		assertEquals("-74.0059728", resultTable.findElement(By.cssSelector("tbody td:nth-child(4)")).getText());
+		dataTextArea.sendKeys("2 | 40°26′47″N,74° 0' 21.5022\"W");
+		submit.click();
+				
+		assertEquals("40.4463889", resultTableBody.findElement(By.cssSelector("td:nth-child(3)")).getText());
+		assertEquals("-74.0059728", resultTableBody.findElement(By.cssSelector("td:nth-child(4)")).getText());
 	}
 	
 	@Test
 	public void testDatesPage() {
 		browser.get(TESTING_SERVER_URL + "dates");
 		
-		browser.findElement(By.name("data")).sendKeys("2 VII 1986");
-		browser.findElement(By.tagName("input")).click();
+		//bind the WebElement to the current page
+		PageFactory.initElements(browser, this);
 		
-		WebElement resultTable = browser.findElement(By.className("narwhal-results"));
+		dataTextArea.sendKeys("2 VII 1986");
+		submit.click();
 		
-		assertEquals("1986", resultTable.findElement(By.cssSelector("tbody td:nth-child(2)")).getText());
-		assertEquals("7", resultTable.findElement(By.cssSelector("tbody td:nth-child(3)")).getText());
-		assertEquals("2", resultTable.findElement(By.cssSelector("tbody td:nth-child(4)")).getText());
-		assertEquals("1986-07-02", resultTable.findElement(By.cssSelector("tbody td:nth-child(5)")).getText());
+		assertEquals("1986", resultTableBody.findElement(By.cssSelector("td:nth-child(2)")).getText());
+		assertEquals("7", resultTableBody.findElement(By.cssSelector("td:nth-child(3)")).getText());
+		assertEquals("2", resultTableBody.findElement(By.cssSelector("td:nth-child(4)")).getText());
+		assertEquals("1986-07-02", resultTableBody.findElement(By.cssSelector("td:nth-child(5)")).getText());
 	}
 	 
 	@After
 	public void tearDown() {
 		browser.close();
 	}
-
 }
